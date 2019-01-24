edec9835f618325b607ad933bcfaea62031f75a8
Custerx <roger.hurtig@hotmail.com>
2019-01-24 11:21:07 +0100

    updated the reducer with data_loaded and replaced strings with constants

---
+++ src/js/constants/message-strings.js
@@ -0,0 +1,2 @@
+export const OK = 'Article successfully added'
+export const ERROR = 'You word is not allowed'
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,26 +1,29 @@
-import { ADD_ARTICLE, FOUND_BAD_WORD } from '../constants/action-types'
+import { ADD_ARTICLE, FOUND_BAD_WORD, DATA_LOADED } from '../constants/action-types'
+import { OK, ERROR } from '../constants/message-strings'
 
 const initialState = {
-  articles: []
-}
-
-const Message = {
-  ok: 'Article successfully added',
-  error: 'You word is not allowed'
+  articles: [],
+  remoteArticles: []
 }
 
 function rootReducer (state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
     return Object.assign({}, state, { // Keeps the original state immutable.
       articles: state.articles.concat(action.payload), // Keeps the initial array immutable.
-      message: Message.ok
+      message: OK
     })
   }
 
   if (action.type === FOUND_BAD_WORD) {
     return Object.assign({}, state, {
       articles: state.articles,
-      message: Message.error
+      message: ERROR
+    })
+  }
+
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
     })
   }
 
